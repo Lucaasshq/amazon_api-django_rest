176a8bd3072579cc8ea21744af2392da8a75cdcc
Lucaasshq <Lucashenriquebarros2016@gmail.com>
2026-04-22 15:13:26 -0300

    .

--- amazon/settings.py
+++ amazon/settings.py
@@ -79,12 +79,14 @@ WSGI_APPLICATION = 'amazon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'amazon_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+       ## 'ENGINE': 'django.db.backends.postgresql',
+      ##  'NAME': 'amazon_db',
+       ## 'USER': 'postgres',
+       ## 'PASSWORD': 'postgres',
+       ## 'HOST': 'localhost',
+       ## 'PORT': '5432',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3', 
     }
 }
 
