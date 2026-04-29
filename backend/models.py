505b859b4facb73ba646fcbe4bd308e6c8edef6f
LUCAS HENRIQUE BARROS MEDEIROS <20242014040042@ifrn.local>
2026-04-29 15:40:44 -0300

    feat: implementando models e serializs

--- backend/models.py
+++ backend/models.py
@@ -42,7 +42,6 @@ class Vendedor(models.Model):
         
 
 class Produto(models.Model):
-
     CATEGORIA_CHOICES = {
         ('eletronicos', 'Eletrônicos'),
         ('roupas', 'Roupas e Acessórios'),
@@ -70,3 +69,83 @@ class Produto(models.Model):
         
     def __str__(self):
         return  f'[{self.nome} — R$ {self.preco}'
+
+
+class PerfilVendedor(models.Model):
+    vendedor = models.OneToOneField(
+        Vendedor,
+        on_delete=models.CASCADE,
+        related_name='perfil',
+        primary_key=True # usa o id vendendor como PK
+    )
+    razao_social = models.CharField(max_length=150, blank=True)
+    inscricao_estadual = models.CharField(max_length=20, blank=True)
+    banco = models.CharField(max_length=50, blank=True)
+    agencia = models.CharField(max_length=10, blank=True)
+    conta = models.CharField(max_length=20, blank=True)
+    chave_pix = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        db_table = 'perfis_vendedores'
+
+    def __str__(self):
+        return f'Perfil de {self.vendedor.nome}'
+    
+    
+class Pedido(models.Model):
+    STATUS_CHOICES = [
+        ('pendente', 'Pendente'),
+        ('pago', 'Pago'),
+        ('enviado', 'Enviado'),
+        ('entregue', 'Entregue'),
+        ('cancelado', 'Cancelado'),
+    ]
+
+    cliente = models.ForeignKey(
+        Cliente,
+        on_delete=models.PROTECT,
+        related_name='pedidos'
+    )
+    
+    status = models.CharField(
+    max_length=20,
+    choices=STATUS_CHOICES,
+    default='pendente'
+    )
+
+    data_pedido = models.DateTimeField(auto_now_add=True)
+    observacoes = models.TextField(blank=True)
+    
+    class Meta:
+        db_table = 'pedidos'
+        ordering = ['-data_pedido']
+    def __str__(self):
+        return f'Pedido #{self.id} — {self.cliente.nome}'
+        
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(
+        Pedido,
+        on_delete=models.CASCADE, # itens não existem sem o pedido
+        related_name='itens'
+    )
+    produto = models.ForeignKey(
+        Produto,
+        on_delete=models.PROTECT, # produto com vendas não pode ser apagado
+        related_name='itens_vendidos'
+   )
+    quantidade = models.PositiveIntegerField(default=1)
+    preco_unitario = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        help_text='Preço congelado no momento da compra'
+    )
+    class Meta:
+        db_table = 'itens_pedido'
+        unique_together = ['pedido', 'produto'] # mesmo produto não duplica no
+    
+    def __str__(self):
+        return f'{self.quantidade}x {self.produto.nome}'
+    
+    @property
+    def subtotal(self):
+        return self.quantidade * self.preco_unitario
